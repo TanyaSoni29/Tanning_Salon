9549aa05806773cc029765751c005339d4dc4822
TanyaSoni2912 <tanu139sona@gmail.com>
2024-09-24 18:07:19 +0530

    Create New customer from dashBoard

--- src/layouts/dashboard/components/CustomerOverview.js
+++ src/layouts/dashboard/components/CustomerOverview.js
@@ -6,12 +6,17 @@ import Modal from "../../../components/Modal";
 import QuestionCustomer from "../components/QuestionCustomer";
 import ProfileModal from "../components/ProfileModal";
 import { Button } from "@mui/material";
+import CreateCustomerModal from "components/CustomerActionButton/CreateCustomerModal";
 function CustomerOverview({ searchQuery, filteredUsers }) {
   const { users } = useSelector((state) => state.profile);
   const [isQuesModal, setIsQuesModal] = useState(false);
   const [profileModal, setProfileModal] = useState(false);
   const [selectedUser, setSelectedUser] = useState({});
-
+  const [createModalOpen, setCreateModalOpen] = useState(false);
+  const handleCreateModalClose = () => setCreateModalOpen(false);
+  const handleCreateNewCustomer = () => {
+    setCreateModalOpen(true);
+  };
   const handleSelectedProfile = () => {
     setProfileModal(true);
     setIsQuesModal(false);
@@ -134,11 +139,42 @@ function CustomerOverview({ searchQuery, filteredUsers }) {
           handleSelectedProfileModal={handleSelectedProfile}
         />
       </Modal>
+      <Modal open={createModalOpen} setOpen={setCreateModalOpen}>
+        <CreateCustomerModal onClose={handleCreateModalClose} />
+      </Modal>
       <Modal setOpen={setProfileModal} open={profileModal}>
         <ProfileModal onClose={handleCloseSelectedProfile} selectedUser={selectedUser} />
       </Modal>
       <div className="bg-white rounded-lg shadow-md p-4">
-        <h2 className="text-lg font-medium mb-4">Customer Overview</h2>
+        <div className="flex justify-between items-center w-full mb-2">
+          <h2 className="text-lg font-medium mb-4">Customer Overview</h2>
+          <Button
+            sx={{
+              backgroundColor: "#328BED",
+              color: "#fff",
+              fontSize: "10px",
+              textTransform: "capitalize",
+              "&:hover": {
+                backgroundColor: "#63A0F5",
+                color: "#fff",
+              },
+              "&:focus": {
+                color: "#fff",
+              },
+              "&:active": {
+                color: "#fff",
+              },
+              "&:disabled": {
+                backgroundColor: "#D3D3D3",
+                color: "#fff",
+              },
+            }}
+            onClick={handleCreateNewCustomer}
+          >
+            Register New Customer
+          </Button>
+        </div>
+
         <div className="overflow-x-auto rounded-lg">
           <table className="min-w-full table-auto border-collapse border border-gray-300 text-[1rem]">
             <thead>
